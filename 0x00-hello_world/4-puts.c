e6cbade95cd0b024ddb7c883d75618c7aec29921
hajargasmi <hajargasmi.contact@gmail.com>
2023-06-15 15:32:23 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* this programme afficher une text inserer au dessiu */
+/* main -A program that prints a line with puts function 
+* Return:0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
