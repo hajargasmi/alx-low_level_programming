89fb5dc95ab78f37c49a3e2316f65e2682cf8060
hajargasmi <hajargasmi.contact@gmail.com>
2023-06-15 15:22:29 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/* this programme afficher une text inserer au dessiu */
 int main(void)
 {
-puts("\" Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
